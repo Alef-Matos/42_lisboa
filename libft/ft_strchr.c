de2173f7c12f6c61918269c245538acc944f3efd
almatos <almatos@student.42.fr>
2022-08-05 19:05:38 +0100

    05/08 19:05

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/05 15:34:28 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/05 17:57:58 by almatos          ###   ########.fr       */
+/*   Created: 2022/08/05 17:13:45 by almatos           #+#    #+#             */
+/*   Updated: 2022/08/05 17:57:26 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-
-#include<unistd.h>
-#include<stdlib.h>
-
-size_t  ft_strlen(const char *s);
-char    *ft_strchr(const char *s, int c);
-
-#endif
+char *ft_strchr(const char *s, int c)
+{
+    while (*s != '\0')
+    {
+      s++;
+      if (c == *s)
+        return ((char *)s);
+    }
+    return (0);
+}
---
+++ libft/man_strchr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   man_strchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 16:42:17 by almatos           #+#    #+#             */
+/*   Updated: 2022/08/05 19:03:51 by almatos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include"libft.h"
+#include <string.h>
+
+int main (void)
+{
+    char    test[] = "abac&te"; 
+    char    *letra;
+    char    *letra2;  
+    int i;
+
+    i = '&';
+        letra = strchr(test, i);
+        letra2 = ft_strchr(test, i);
+        write(1, "\n", 1);
+        write(1, "A palavra é abacate", 20);
+        write(1, " :\n\n", 4);
+        write(1, "Função strchr: ", 17);
+        write(1, letra, 1);
+        write(1, "\n", 1);
+        write(1, "Função ft_strchr: ", 20);
+        write(1, letra2, 1);
+        write(1, "\n\n", 2);
+}
