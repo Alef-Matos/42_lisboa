88fe4dd20f0130bb8137bff957a4dbbd923d659f
almatos <almatos@student.42.fr>
2022-08-05 16:38:14 +0100

    05/08

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/05 15:46:40 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/05 16:15:20 by almatos          ###   ########.fr       */
+/*   Created: 2022/08/05 15:43:06 by almatos           #+#    #+#             */
+/*   Updated: 2022/08/05 16:09:43 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
-#include"libft.h"
-#include <string.h>
 
-int main (void)
+size_t ft_strlen(const char *s)
 {
-   
-    char test[] = "abacate";
-    int n;
-    int n2;
+    size_t i;
 
-    n2 = strlen(test) + '0';
-    n = ft_strlen(test) + '0';
-    write(1, &n, 1);
-    write(1, "\n", 1);
-    write(1, &n2, 1);
-    write(1, "\n", 1);
+    i = 0;
+    while(s[i])
+        i++;
+        return (i);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -1,30 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/05 15:46:40 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/05 16:15:20 by almatos          ###   ########.fr       */
+/*   Created: 2022/08/05 15:34:28 by almatos           #+#    #+#             */
+/*   Updated: 2022/08/05 16:17:33 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+#define LIBFT_H
+
 #include<unistd.h>
-#include"libft.h"
-#include <string.h>
+#include<stdlib.h>
 
-int main (void)
-{
-   
-    char test[] = "abacate";
-    int n;
-    int n2;
+size_t  ft_strlen(const char *s);
 
-    n2 = strlen(test) + '0';
-    n = ft_strlen(test) + '0';
-    write(1, &n, 1);
-    write(1, "\n", 1);
-    write(1, &n2, 1);
-    write(1, "\n", 1);
-}
+#endif
