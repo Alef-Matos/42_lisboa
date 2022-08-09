5ccb5c7558d3bf0ab3e6ae31d9930dae2dc9fea3
almatos <almatos@student.42.fr>
2022-08-09 23:51:46 +0100

    estudos

--- libft/test_strlcat.c
+++ libft/test_strlcat.c
@@ -6,24 +6,56 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 16:56:00 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/09 23:13:59 by almatos          ###   ########.fr       */
+/*   Updated: 2022/08/09 23:50:38 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+
+
+size_t ft_strlen(const char *s)
+{
+    size_t  l;
+
+    l = 0;
+    while(*s++)
+        l++;
+    return (l);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t c;
+	size_t d;
+
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	c = ft_strlen(dst);
+	d = 0;
+	while (src[d] != '\0' && c + 1 < dstsize)
+	{
+		dst[c] = src[d];
+		c++;
+		d++;
+	}
+	dst[c] = '\0';
+	return (ft_strlen(dst) + ft_strlen(&src[d]));
+}
 
 int main (void)
 {
-    char dst[] = "Costa";
-    char src[] = "Alef";
+    char dst[] = "42";
+    char src[] = "lisboa";
     int tamanho;
     int numero;
 
-    numero = 4;
+    numero = 9;
     printf("\nEsta função trabalha com String verdadeira, ou seja conta o caracter vazio no final da string\n\n");
-    printf("Sorce: %s \nResultado do Destino antes da função: %s\n", src, dst);
-    tamanho = strlcat(dst, src, numero);
+    printf("Destino: %s \nSource: %s\n", dst, src);
+    printf("Destino + Source + '\\0': %s %s\n", dst, src);
+    tamanho = ft_strlcat(dst, src, numero);
     printf("Resultado do Destino depois da função incluindo o algoritimo vazio: %s\n", dst);
-    printf("Tamanho do Sorce incluindo algoritimo vazio: %d\n\n", tamanho);
+    printf("Tamanho do Sorce excluindo o ultimo caracter '\\0': %d\n\n", tamanho);
 }
--- libft/test_strlcpy.c
+++ libft/test_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 16:56:00 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/09 22:14:31 by almatos          ###   ########.fr       */
+/*   Updated: 2022/08/09 23:51:22 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ int main (void)
 //printf("Sorce: %s \nResultado do Destino antes da função: %s\n", src, dst);
     tamanho = ft_strlcpy(dst, src, numero);
     printf("Resultado do Destino depois da função incluindo o algoritimo vazio: %s\n", dst);
-    printf("Tamanho do Sorce incluindo algoritimo vazio: %d\n\n", tamanho);
+    printf("Tamanho do Sorce excluindo o algoritimo vazio: %d\n\n", tamanho);
 }
