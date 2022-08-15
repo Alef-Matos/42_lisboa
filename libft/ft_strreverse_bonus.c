190803ada51c73becff7ad29d74bf8a4092a9cff
almatos <almatos@student.42.fr>
2022-08-15 16:41:53 +0100

    15/08 16:41

---
+++ libft/ft_strreverse_bonus.c
@@ -0,0 +1,20 @@
+#include <"libft".h>
+
+char *ft_strreverse(char *str)
+{
+    int cont;
+    int i;
+    char    swap;
+    
+    cont = ft_strlen(str);
+    i = 0;
+    while(str[i] && i <= cont -1)
+    {
+        swap = str[i];
+        str[i] = str[cont -1];
+        str[cont -1] = swap;
+        i++;
+        cont--;
+    }  
+    return (str);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 15:34:28 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/10 22:58:47 by almatos          ###   ########.fr       */
+/*   Updated: 2022/08/15 16:20:29 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+** Part 1 - Libc functions
+*/
+
 size_t  ft_strlen(const char *s);
 char    *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+/*
+** Extra functions
+*/
+
+char *ft_strreverse(char *str);
 
 #endif
