4cb1997eddf9aab73e9e7c6e93f47bfcd9341eb6
almatos <almatos@student.42.fr>
2022-08-18 10:01:18 +0100

    18/08 10:00

--- libft/main_test.c
+++ libft/main_test.c
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:04:35 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/17 20:34:39 by almatos          ###   ########.fr       */
+/*   Updated: 2022/08/17 21:08:24 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 #include <string.h>
 
 //COPIAR TESTES DAS FUNÇÕES ABAIXO
-//TESTE DA FUNÇÃO FT_STRSTR_BONUS
-int main(void)
+int main (void)
 {
-    char    *s1 = "abcabdooo";
-    char    *s2 = "abd";
-    char    *i2 = ft_strstr(s1, s2);
-    printf("\nString: %s\nFind: %s\nResult: %s\n", s1, s2, i2);
-    return 0;
+    char dst[5];
+    char src[] = "Alef";
+    int tamanho;
+    int numero;
+
+    numero = 4;
+    printf("\nEsta função trabalha com String verdadeira, ou seja conta o caracter vazio no final da string\n\n");
+    tamanho = ft_strlcpy(dst, src, numero);
+    printf("Resultado do Destino depois da função incluindo o algoritimo vazio: %s\n", dst);
+    printf("Retorno:Tamanho do  Destino + Source excluindo o ultimo caracter '\\0': %d\n\n", tamanho);
 }
