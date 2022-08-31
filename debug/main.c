22d93f2651e26b1c63ecc92d05d08da359bea85b
almatos <almatos@student.42.fr>
2022-08-31 18:36:33 +0100

    31/08 18:36

--- debug/main.c
+++ debug/main.c
@@ -1,28 +1,4 @@
 #include <stdio.h>
 #include <string.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t i;
-
-	if (!dst && !src)
-		return (0);
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)dst)[i, i++] = ((unsigned char *)src)[i];
-//		i++;
-//        *(unsigned char*)(dst + i++) = *(unsigned char*)(src++);
-	}
-	return (dst);
-}
-
-//COPIAR TESTES DAS FUNÇÕES ABAIXO
-int main (void)
-{
-    char dest[] = "alef";
-    const char source[] = "Costa Matos";
-
-    ft_memcpy(dest, source, strlen(dest));
-    printf("ft_memcpy -> Source: %s\nResultado do Destino depois da função: %s\n\n", source, dest);
-}
+//COPIAR FUNÇÕES COM MAIM PARA DEBUG
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 15:15:32 by almatos           #+#    #+#             */
-/*   Updated: 2022/08/31 17:23:54 by almatos          ###   ########.fr       */
+/*   Updated: 2022/08/31 18:35:20 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t index;
 
 	if (!dst && !src)
 		return (0);
-	i = 0;
-	while (i < n)
+	index = 0;
+	while (index < n)
 	{
-		*(unsigned char*)(dst + i++) = *(unsigned char*)(src++);
+		*(unsigned char*)(dst + index++) = *(unsigned char*)(src++);
 	}
 	return (dst);
 }
