7051c5b40980172ebb61267e82884509803d0393
almatos <almatos@student.42.fr>
2022-08-31 19:26:19 +0100

    31/08 19:30

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t index;
+	size_t	index;
 
 	if (!dst && !src)
 		return (0);
 	index = 0;
 	while (index < n)
 	{
-		*(unsigned char*)(dst + index++) = *(unsigned char*)(src++);
+		*(unsigned char *)(dst + index++) = *(unsigned char *)(src++);
 	}
 	return (dst);
 }
