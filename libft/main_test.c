b35e5b68946780a3a8696b2ee5cd8be08107efd1
almatos <almatos@student.42.fr>
2022-09-04 00:45:10 +0100

    04/09 00:45

--- libft/main_test.c
+++ libft/main_test.c
@@ -6,59 +6,60 @@
 /*   By: almatos <almatos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:04:35 by almatos           #+#    #+#             */
-/*   Updated: 2022/09/01 19:04:54 by almatos          ###   ########.fr       */
+/*   Updated: 2022/09/04 00:28:04 by almatos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
 //COPIAR TESTES DAS FUNÇÕES ABAIXO
-//FUNÇÃO FT_MENCHR.C
-#include <stdio.h>
-#include <string.h>
-int main (void)
+//FUNÇÃO FT_pritoposto.C
+void pritoposto(char *str)
 {
-    char    fonte[50];
-    char     l;
-    char    *ponteiro_1;
-    char    *ponteiro_2;
-    int     n;
-    int     letra;
-
-    printf("\n######### BUSCA DE UM CARACTER NUM BLOCO DE MEMORIA E RETORNA UM PONTEIRO NA POSIÇÃO ENCONTRADA #########\n\n");
-    printf("String fonte: ");
-    fgets(fonte,50,stdin);
-    printf("Letra para busca: ");
-    scanf("%c", &l);
-    printf("Número de bits: ");
-    scanf("%d", &n);
-
-    letra = l;
-    ponteiro_1 = (char *)ft_memchr(fonte, letra, n);
-    ponteiro_2 = (char *)memchr(fonte, letra, n);
-    printf("\n==>Função memchr:\n");
-    printf("String fonte = %sLetra para busca = %c\nNumero de bits: %d", fonte, letra, n);
-    if(ponteiro_2)
+    int index;
+    int posi;
+    char    alfa[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char    alfa_inverso[] = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+    
+    index = 0;
+    while(str[index])
+    {
+        posi = 0;
+        if (str[index] >='A' && str[index] <= 'Z')
         {
-            printf("\nLetra %c, encontrado na posição %ld\n", letra, ponteiro_2 - fonte + 1);
-            printf("Estring a partir da letra encontrada: %s\n", ponteiro_2);
+            while(posi <= 25 && str[index] != alfa[posi])
+                posi++;
+            ft_putchar(alfa_inverso[posi]);
+        }
+        else if(str[index] >='a' && str[index] <= 'z')
+        {
+            while(posi <= 25 && str[index] != alfa[posi] + 32)
+                posi++;
+            ft_putchar(alfa_inverso[posi]+32);
+        }
+        else
+            write(1, &str[index], 1); 
+        index++;
+    }
+}
+
+int main (int argc, char **argv)
+{
+    int index;
+    
+    index = 1;
+    if (argc >= 2)
+      {
+           while(argc > index)
+            {
+                pritoposto(argv[index]);
+                if (argc > index +1)
+                    write(1, " ", 1);
+                if (argc == index +1)
+                    write(1, "\n", 1);
+                index++;                
+            }         
         }
     else
-        printf("\nLetra %c, não encontrado!\n", letra);
-    printf("\n==>Função ft_memcmp:\n");
-    printf("String fonte = %sLetra para busca = %c\nNumero de bits: %d", fonte, letra, n);
-    if(ponteiro_1)
-        {
-            printf("\nLetra %c, encontrado na posição %ld\n", letra, ponteiro_1 - fonte + 1);
-            printf("Estring a partir da letra encontrada: %s\n", ponteiro_1);
-        }
-    else
-        printf("\nletra %c, não encontrado!\n\n", letra);
-    if(ponteiro_1 == ponteiro_2)
-        printf(":::::::: MOULINETTE OK ::::::::\n");
-    else
-        printf(":::::::: Funções diferentes ::::::::::\n");
-    return (0);
+        write(1, "Falta argumentos\n", 18); 
 }
